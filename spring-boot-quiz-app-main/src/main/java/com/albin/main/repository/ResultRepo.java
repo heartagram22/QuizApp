5e233ce6637dd2e73fe9004a87a79c59762db551
Albin Gjonaj <92172913+heartagram22@users.noreply.github.com>
2022-01-14 16:31:51 +0100

    quiz app

---
+++ spring-boot-quiz-app-main/src/main/java/com/albin/main/repository/ResultRepo.java
@@ -0,0 +1,10 @@
+package com.albin.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.albin.main.model.Result;
+
+@Repository
+public interface ResultRepo extends JpaRepository<Result, Integer> {
+	
+}
